ce4846d73d07b6a6bb48ecb2e104bb16365a5cb9
RenSyntax <ren.syntax@gmail.com>
2026-06-02 22:24:42 +0530

    Cin function

--- Basics/pattern.cpp
+++ Basics/pattern.cpp
@@ -2,16 +2,22 @@
 using namespace std;
 
 // Problem 1: Printing a 4x4 pattern:
-int main() {
+void pattern(int n) {
   // step 1: to focus on outer loops which are for rows
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < n; i++) {
 
     // step 2: focus on inner loop for columns.
-    for (int j = 0; j < 4; j++) {
+    for (int j = 0; j < n; j++) {
       cout << "*";
     }
     cout << endl; // the change the line.
   }
+}
 
+int main() {
+  int input{};
+  cout << "Please Enter the Number of Symetry: ";
+  cin >> input;
+  pattern(input);
   return 0;
 }
