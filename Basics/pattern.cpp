25d2b03a7fd74cf1fe2f6895d1940dc15de08d7c
RenSyntax <ren.syntax@gmail.com>
2026-06-02 21:59:00 +0530

    4x4 Pattern

--- Basics/pattern.cpp
+++ Basics/pattern.cpp
@@ -2,3 +2,15 @@
 using namespace std;
 
 // Problem 1: Printing a 4x4 pattern:
+int main() {
+  // step 1: to focus on outer loops which are for rows
+  for (i = 0; i < 4; i++) {
+
+    // step 2: focus on inner loop for columns.
+    for (j = 0; j < 4; j++) {
+      cout << "*";
+    }
+  }
+  cout << endl; // the change the line.
+  return 0;
+}
