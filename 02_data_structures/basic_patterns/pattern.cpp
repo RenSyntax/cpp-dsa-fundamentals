186390e7cf4d57557fefd8cc805604ba5e1e1628
RenSyntax <ren.syntax@gmail.com>
2026-07-01 13:46:35 +0530

    Pattern 7

--- 02_data_structures/basic_patterns/pattern.cpp
+++ 02_data_structures/basic_patterns/pattern.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Pattern 1:
+// Pattern 1: 
 void pattern1(int n) {
-
   /*
-  Example
-  *****
-  *****
-  *****
+  Example:
+  ****
+  ****
+  ****
+  ****
   */
+
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       cout << "*";
     }
     cout << endl;
   }
-  // Done.
+
 }
 
 // Pattern 2:
 void pattern2(int n) {
-
   /*
   Example:
   *
@@ -30,14 +30,12 @@ void pattern2(int n) {
   ****
   */
 
-  for (int i = 1; i <= n; i++) { // If you do 'int i = 1' instead of '0', make
-                                 // sure to use '<=' rather than just '<'
-    for (int j = 1; j <= i; j++) {
+  for (int i = 1; i <= n; i++) {        // If you do 'int i = 1' instead of '0', make
+        for (int j = 1; j <= i; j++) {  // sure to use '<=' rather than just '<'
       cout << "*";
     }
     cout << endl;
   }
-  // Done.
 }
 
 // Pattern 3:
@@ -53,14 +51,10 @@ void pattern3(int n) {
 
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= i; j++) {
-      cout
-          << j; // Try to Visualize the logic with a example input.
-                // When you use 'j' you are calling the nested loop, whose value
-                // differ, and is not constant, example: 1 --> 2 --> 3......
+      cout << j;
     }
     cout << endl;
   }
-  // Done.
 }
 
 // Pattern 4:
@@ -76,9 +70,7 @@ void pattern4(int n) {
 
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= i; j++) {
-      cout << i; // outside 'for' loop determines the rows, when you use it
-                 // inside a nested loop, it acts as a constant, example '1',
-                 // '2' or '3'....
+      cout << i;
     }
     cout << endl;
   }
@@ -95,10 +87,58 @@ void pattern5(int n) {
   *
   */
 
-  for (int i = 0; i <= n; i++) {
-    for (int j = 0; j <= n; j++) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n-i+1; j++) {
       cout << "*";
     }
+    cout << endl;
+  }
+}
+
+// Pattern 6:
+void pattern6(int n) {
+  /*
+  Example:
+  12345
+  1234
+  123
+  12
+  1
+  */
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n-i+1; j++) {
+      cout << j;
+    }
+    cout << endl;
+  }
+}
+
+// Pattern 7:
+void pattern7(int n) {
+  /*
+  Example:
+        *
+       ***
+      *****
+     *******
+    *********    
+  */
+
+  for (int i = 0; i < n; i++) {
+
+    for (int j = 1; j <= n-i-1; j++) { // for space on left side.
+      cout << " ";
+    }
+    for (int j = 1; j <= (2*i+1); j++) { // for stars in center.
+      cout << "*";
+    }
+    for (int j = 1; j <= n-i-1; j++) { // for space on right side.
+      cout << " ";
+    }
+
+    cout << endl;
+    
   }
 }
 
@@ -106,6 +146,6 @@ void pattern5(int n) {
 int main() {
   int input{};
   cin >> input;
-  pattern4(input);
+  pattern7(input);
   return 0;
 }
