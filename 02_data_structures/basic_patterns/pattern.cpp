1a67fbe7f41b0287666d8cf1403f48073fa3e928
RenSyntax <ren.syntax@gmail.com>
2026-07-02 22:34:21 +0530

    Daily Practice Completed

--- 02_data_structures/basic_patterns/pattern.cpp
+++ 02_data_structures/basic_patterns/pattern.cpp
@@ -195,7 +195,27 @@ void pattern5(int n) {
 }
 
 void pattern6(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n-i+1; j++) {
+      cout << j;
+    }
+    cout << endl;
+  }
+}
 
+void pattern7(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n-i-1; j++) {
+      cout << " "
+    }
+    for (int j = 0; j < 2*i+1; j++) {
+      cout << "*"
+    }
+    for (int j = 0; j < n-i-1; j++) {
+      cout << " "
+    }
+    cout << endl;
+  }
 }
 
 // backend:
