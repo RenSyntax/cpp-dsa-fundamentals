4b2f3fb98032e0c83f76cd5134b75d524d74c8f5
RenSyntax <ren.syntax@gmail.com>
2026-07-11 22:11:17 +0530

    Pattern 9

---
+++ 02_data_structures/basic_patterns/pattern_9.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void pattern7(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2*i+1; j++) {
+            cout << "*";
+        }
+        for (int j = 0; j < n-i-1; j++) {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern8(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < 2*n-2*i+1; j++) {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int input{};
+    cout << "Please Enter n" << endl;
+    cin >> input;
+    pattern7(input);
+    pattern8(input);
+}
