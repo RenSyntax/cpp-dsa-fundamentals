ed49cee09b6cac491136824b4c2131ea730fdaba
RenSyntax <ren.syntax@gmail.com>
2026-07-15 23:03:38 +0530

    Pattern 18

---
+++ 02_data_structures/basic_patterns/pattern_18.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void pattern18(int n) {
+    for (int i = 0; i < n; i++) {
+        for (char ch = 'E' - i; ch <= 'E'; ch++) {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int input{};
+    cout << "Please Enter n" << endl;
+    cin >> input;
+    pattern18(input);
+}
