20f73da381b38734285df8f7d24e804390b23fd9
RenSyntax <ren.syntax@gmail.com>
2026-07-18 23:31:22 +0530

    Accessing Elements in vector & Prining of vector

--- 01_cpp_fundamentals/standard_templete_library/vector.cpp
+++ 01_cpp_fundamentals/standard_templete_library/vector.cpp
@@ -11,6 +11,7 @@ void vector() {
     v.emplace_back{3} // Adds '3' to the countainer just after '1'.
     // The final Countainer looks like ---> {1, 3}
 
+// ---
 
     // Pair inside a vector
     vector <pair <int, int>> v2;
@@ -20,4 +21,41 @@ void vector() {
     vector <int> v3{5, 100}; // Countainer containing '100' five times already defined.
     vector <int> v4{5}; // Container containing garbage value five times already defined.
     vector <int> v5{v3}; // Copy of vector v3;
+
+// ---
+
+    // ---> How to access elements in a vector ? take example vector, v = {1, 3, 5, 7}
+
+    // First way: Similar to Arrays
+    cout << v[0]; // Prints '1'
+    cout << v[1]; // Prints '2'
+    // and so on....
+
+    // Second way: Using iterater
+    vector<int>::iterater it = v.begin(); // Memory Location of first element.
+    cout << *(it); // Prints the first element. --> '1'
+    it++; // Memory location moved to next one, AKA Memory location of second element.
+    cout << *(it); // Prints the second element. ---> '3'
+    // and so on.....
+    v.end(); // Memory location of one element after the last element.
+    v.back(); // Memory location of last element.
+
+// ---
+
+    // ---> How to print a vector ?
+
+    for (vector<int>::iterater it = v.begin(); it != v.end(); it++) { // Prints whole vector.
+        cout << *(it) << " "; // You can change 'v.end()' to something else, if you want to print part of vector.
+    }
+
+    // Shorter way:
+    for (auto it = v.begin(); it != v.end(); it++) { // 'auto' automatically assigins the data type according to the data.
+        cout << *(it) << " ";
+    }
+
+    // More Shorter way:
+    for (auto it : v) { // Here 'it' is int data type.
+        cout << it << " ";
+    }
+
 }
